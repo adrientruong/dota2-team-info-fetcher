eecc5dfbd0990f362a2cc8d79c40bd0a92de50a9
Adrien Truong <adrien.truong@me.com>
2014-02-01 11:53:59 -0800

    Fix really big numbers being truncated

--- fetchTeamInfos.js
+++ fetchTeamInfos.js
@@ -4,6 +4,7 @@ var RateLimiter = require('limiter').RateLimiter;
 var request = require('request');
 var fs = require('fs');
 var path = require('path');
+var JSONBig = require('json-bigint');
 
 var argv = require('optimist').argv;
 var key = argv.key;
@@ -13,13 +14,16 @@ var limiter = new RateLimiter(1, 1000);
 var getTeamInfo = function(teamID, callback) {
 	limiter.removeTokens(1, function(error, remainingRequests) {
 		var url = 'http://api.steampowered.com/IDOTA2Match_570/GetTeamInfoByTeamID/v1/?key=' + key + '&start_at_team_id=' + teamID;
-		var options = { url: url, json: true };
+		var options = { url: url };
 		request(options, function(error, response, body) {
 			if (!error && response.statusCode == 200) {
-				var teams = body.result.teams;
+				var parsedBody = JSONBig.parse(body);
+				var teams = parsedBody.result.teams;
 
 				if (teams.length != 0 && teams[0].team_id == teamID) {
 					var teamInfo = teams[0];
+					convertIDsToStrings(teamInfo);
+					
 					callback(null, teamInfo);
 				}
 				else {
@@ -32,6 +36,18 @@ var getTeamInfo = function(teamID, callback) {
 			}
 		});
 	});
+}
+
+var convertIDsToStrings = function(object) {
+	var explicitKeys = ['logo', 'logo_sponsor'];
+	for (var key in object) {
+		if (object.hasOwnProperty(key)) {
+			if (explicitKeys.indexOf(key) != -1 || key.indexOf('_id') != -1) {
+				var stringValue = object[key].toString();
+				object[key] = stringValue;
+			}
+		}
+	}
 }
 
 var pathArgument = process.argv[2];
